16761930554f90220f93fccd5b99bfcdfcd167df
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 03:35:33 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,39 +4,29 @@
  def starting_stance
 puts"Plant legs far apart, bend knees slightly and keep posture loose"
 end 
-
 def base_footwork
 puts "Lift right foot"
 puts "Return right foot to the ground"
-skip_step("left foot")
+skip_step ("left foot")
 end
-
 def skip_step(starting_foot)
 puts "Lower #{starting_foot} to the ground"
 puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
 puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end 
-
-
 def bob_the_reins 
 puts "Holding your arms out in front of you, straight and at chest level"
 puts "Cross your right wrist over your left and hold them together"
 puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 end 
-
-
 def lasso
 puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
 puts "Lift right arm up and point right elbow diagonally to the right."
 puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end 
-
-
 def bust_a_move
 puts "Be cool and be snazzy!"
 end 
-
-
 def gangnam_style
 starting_stance
 base_footwork
