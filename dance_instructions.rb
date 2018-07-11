c7540278d0698676d1cc9673b3d25ad2dca2f54a
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 03:43:30 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -17,7 +17,7 @@ puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
 puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end 
 
-
+def bob_the_reins
 puts "Holding your arms out in front of you, straight and at chest level"
 puts "Cross your right wrist over your left and hold them together"
 puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
